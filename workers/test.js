efc0eeb8519935fd5a5466a9fc70b0c0549d99da
Yuki Akiyama <you2197901@gmail.com>
2017-04-02 18:51:40 +0900

    Update test.js

--- workers/test.js
+++ workers/test.js
@@ -1,7 +1,7 @@
 const x = 100
 const sleep = require('@you21979/promise-sleep');
 
-const main = async (input) => {
+const main = async (input, progress) => {
     console.log(x);
     console.log(process.pid);
     console.log("wait 1sec");
@@ -9,7 +9,7 @@ const main = async (input) => {
 }
 
 module.exports = (input, done, progress) => {
-    main(input, done).then(() => {
+    main(input, progress).then(() => {
        done({ string : input.string, integer : parseInt(input.string), id:input.id });
     })
 }
