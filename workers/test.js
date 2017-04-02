c2219ce02f5aac667fa85b91e1227b5e999a6a33
Yuki Akiyama <you2197901@gmail.com>
2017-04-02 18:53:59 +0900

    Update test.js

--- workers/test.js
+++ workers/test.js
@@ -5,11 +5,22 @@ const main = async (input, progress) => {
     console.log(x);
     console.log(process.pid);
     console.log("wait 1sec");
-    return await sleep(1000);
+    await sleep(1000);
+    return { string : input.string, integer : parseInt(input.string), id:input.id }
 }
 
 module.exports = (input, done, progress) => {
-    main(input, progress).then(() => {
-       done({ string : input.string, integer : parseInt(input.string), id:input.id });
+    main(input, progress).then(res => {
+       done({
+           id : input.id,
+           result : res,
+           status : 1,
+       });
+    }).catch(e => {
+       done({
+           id : input.id,
+           error : e,
+           status : 0,
+       });
     })
 }
